769fd542bf736e7b87066901460a30f438584cd3
GahiwalTejas <tejasgahiwal1999@gmail.com>
2023-12-29 09:58:25 +0530

    Raider_And_AllRounder_Player_By_TeamWise

---
+++ ScoreCardApi/SqlQueries/RaidersAndAllRounder.sql
@@ -0,0 +1,5 @@
+﻿--Players Role Wise And Team Wise
+
+Select FirstName,LastName From Players 
+inner join Teams On (Teams.Id=Players.TeamId)
+where (RoleId=1 Or RoleId=3) and Teams.TeamName='UMumba';
